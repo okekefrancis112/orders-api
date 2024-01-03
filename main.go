9caa26bfc8100d3f7cf0fba76de8412eb65790c0
okekefrancis112 <okeke98@gmail.com>
2024-01-03 18:15:19 +0100

    server abstracted into the application package

--- main.go
+++ main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"context"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/okekefrancis112/orders-api/application"
 )
 
 func main() {
-	// fmt.Println("Hello, world!")
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("failed to listen to server", err)
+		fmt.Println("failed to start app: ", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
 }
